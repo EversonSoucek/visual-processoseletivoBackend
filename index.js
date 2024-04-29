c5923b54f4c0b0b34e5e245ae4d869b459fc164f
Everson <sousekjuniorr@outlook.com>
2024-04-29 00:24:32 -0300

    feat: Muda package json para funcionar com heroku

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const mysql = require('mysql')
-const porta = 3500;
+//const mysql = require('mysql')
+//const porta = 3500;
 
 app.use(express.json())
 app.use(cors())
 
 app.use("/", require('./routes/usuarios'))
 
-app.listen(porta, () => {
+/*app.listen(porta, () => {
     console.log("Servidor rodando");
+})*/
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('rodando');
 })
